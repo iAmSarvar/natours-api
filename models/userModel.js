5899fb48cb48a72045a3aaf6c95e6bfc4222867d
Sarvar Sultanov <iamsarvarsultanov@gmail.com>
2024-06-26 12:02:31 +0500

    deleting current user

--- models/userModel.js
+++ models/userModel.js
@@ -39,7 +39,12 @@ const userSchema = new mongoose.Schema({
   },
   passwordChangedAt: Date,
   passwordResetToken: String,
-  passwordResetExpires: Date
+  passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false
+  }
 });
 
 userSchema.pre('save', function(next) {
@@ -55,6 +60,11 @@ userSchema.pre('save', async function(next) {
   this.password = await bcrypt.hash(this.password, 12);
   this.passwordConfirm = undefined;
 
+  next();
+});
+
+userSchema.pre(/^find/, function(next) {
+  this.find({ active: { $ne: false } });
   next();
 });
 
