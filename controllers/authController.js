6706636b52e9095f1b9d7a77697032bb5bafd723
Sarvar Sultanov <iamsarvarsultanov@gmail.com>
2024-06-26 12:45:16 +0500

    imlemented cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,6 +15,20 @@ const signToken = id => {
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_EXPIRES_COOKIE_IN * 24 * 60 * 60 * 1000 //convert to milliseconds
+    ),
+    httpOnly: true
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  //hiding password when signup
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     token,
