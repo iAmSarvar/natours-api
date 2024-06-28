0a733af99f1dc0711c0d49f729afbfd7a7fb23e5
Sarvar Sultanov <iamsarvarsultanov@gmail.com>
2024-06-28 15:24:55 +0500

    virtual populating on tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -119,6 +119,12 @@ tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
 
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField: 'tour',
+  localField: '_id'
+});
+
 // DOCUMENT MIDDLEWARE: runs before .save() and .create()
 // tourSchema.pre('save', async function(next) {
 //   const guidePromises = this.guides.map(async id => await User.findById(id));
