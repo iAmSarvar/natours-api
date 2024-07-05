62d95835294d851d6200d07bd731fec8d15ba857
Sarvar Sultanov <iamsarvarsultanov@gmail.com>
2024-07-05 16:04:38 +0500

    overview page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,13 @@
-const getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+const getOverview = catchAsync(async (req, res) => {
+  const tours = await Tour.find();
   res.status(200).render('overview', {
-    title: 'All tours'
+    title: 'All tours',
+    tours
   });
-};
+});
 
 const getTour = (req, res) => {
   res.status(200).render('tour', {
